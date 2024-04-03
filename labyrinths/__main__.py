d387083c24b1d2d31e1ed5bb79e7bb2273f847ed
Roman Trapeznikov <me@rom4ik.su>
2024-04-04 01:18:23 +0300

    tweak readme

--- labyrinths/__main__.py
+++ labyrinths/__main__.py
@@ -12,24 +12,24 @@ def main() -> None:
     parser = argparse.ArgumentParser(prog="labyrinths", description="generate or solve mazes")
     parser.add_argument("-v", "--visual", help="run GUI", action="store_true")
     # parser.add_argument('--loadfrom', help='load maze into visualizer window', type=argparse.FileType())
-    parser.add_argument("--loadfrom", help="load maze into GUI window", metavar='FILE')
+    parser.add_argument("--loadfrom", help="load maze into GUI window", metavar="FILE")
 
     parser.add_argument("--generate", help="generate and save maze into file", metavar="FILE")
     parser.add_argument("--size", help="select maze size for generation", default="59,39")
-    parser.add_argument("--algo", help="select generation algorithm", default="mst", nargs='?', choices=["mst", "dfs"])
+    parser.add_argument("--algo", help="select generation algorithm", default="mst", nargs="?", choices=["mst", "dfs"])
     args = parser.parse_args()
 
     if args.visual:
-        from labyrinths.ui.widgets.mazewindow import MazeWidget
         from labyrinths.ui.mainwindow import MainWindow
+        from labyrinths.ui.widgets.mazewindow import MazeWidget
 
         mainwindow = MainWindow(800, 600)
         mazewidget = MazeWidget(mainwindow.root_widget, 800, 600, 0, 0)
         mazewidget.new_maze()
         mainwindow.run()
     elif args.generate:
-        from labyrinths.generators.kruskal import KruskalGenerator
         from labyrinths.generators.dfs import DepthFirstSearchGenerator
+        from labyrinths.generators.kruskal import KruskalGenerator
         from labyrinths.mazeloader import dump_maze
 
         dest = args.generate
